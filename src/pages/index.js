58e5dd442cfc52a035babcea7a06968b1cd659b0
Artur Almeida <artuzinhotutu00@hotmail.com>
2021-05-15 15:40:01 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,27 @@
 import { Box, Center, Grid } from "@chakra-ui/layout";
-import {Heading}from "@chakra-ui/react"
-import { Image } from '@chakra-ui/image'
+import { Heading } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/image";
 import { Container } from "../components/Container";
-import { pokedex } from '../pokemon.json-master/pokedex'
-
-
-
+import { pokedex } from "../pokemon.json-master/pokedex";
 
 function replace(a) {
-
-  console.log('teste', a)
+  console.log("teste", a);
 
   if (a < 10) {
-    console.log('/images/00' + a + '.png')
-    return '/images/00' + a + '.png'
+    console.log("/images/00" + a + ".png");
+    return "/images/00" + a + ".png";
   }
   if (a < 100) {
-    return '/images/0' + a + '.png'
+    return "/images/0" + a + ".png";
   }
   if (a < 1000) {
-    return '/images/' + a + '.png'
+    return "/images/" + a + ".png";
   }
-
-
 }
 
 export default function Index() {
-
-
-
   return (
-    <Container height="100vh" >
-
-
-
+    <Container height="100vh">
       <Grid
         bg="white"
         templateColumns="repeat(4, 1fr)"
@@ -43,44 +31,44 @@ export default function Index() {
       >
         {pokedex.map((poke) => (
           <Box>
-            <Center w="200px" h="200" bg="#D9DFD8" flexDirection="column"  borderRadius='20px' >
-
+            <Center
+              w="200px"
+              h="200"
+              bg="#D9DFD8"
+              flexDirection="column"
+              borderRadius="20px"
+            >
               <h1>{poke.name.english} </h1>
               <Image
                 boxSize="100px"
                 objectFit="cover"
                 src={replace(poke.id)}
-                alt="Segun Adebayo" />
+                alt="Segun Adebayo"
+              />
             </Center>
           </Box>
         ))}
       </Grid>
-
     </Container>
-  )
+  );
 }
 
+// export async function getServerSideProps() {
+//   // const result = await fetch('http://localhost:3000/api/listpokemon%27);
 
+//   // const listaPokemon = await result.json();
 
-export async function getServerSideProps() {
+//   // const arrayData = []
 
-  const result = await fetch('http://localhost:3000/api/listpokemon%27);
+//   // listaPokemon.results.map((item)=>{
+//   //   const result = await fetch(item.url);
+//   //   const pokemon = await result.json()
+//   //   arrayData.push(pokemon)
+//   // })
 
-  const listaPokemon = await result.json();
-
-  // const arrayData = []
-
-  // listaPokemon.results.map((item)=>{
-  //   const result = await fetch(item.url);
-  //   const pokemon = await result.json()
-  //   arrayData.push(pokemon)
-  // })
-
-
-  return {
-    props: {
-
-      data: listaPokemon
-    }
-  }
-}
+//   return {
+//     props: {
+//       data: listaPokemon,
+//     },
+//   };
+// }
