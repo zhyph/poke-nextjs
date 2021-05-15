ef5d263661d00d23377a05abe8297ceacb7b8fbf
Artur Almeida <artuzinhotutu00@hotmail.com>
2021-05-15 14:06:53 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,19 @@ const Index = ({ data }) => {
   console.log(data);
 
   return (
-    <Grid bg="black" templateColumns="repeat(4, 1fr)" width="100%" height="100vh" gap={5}>
+    <Grid
+      bg="black"
+      templateColumns="repeat(4, 1fr)"
+      width="100%"
+      height="100vh"
+      gap={5}
+    >
       {data.map((poke) => (
-        <Center w="200px" h="200" bg="red">
-          <h1>{poke.name}</h1>
-        </Center>
+        <Box>
+          <Center w="200px" h="200" bg="red">
+            <h1>{poke.name} ju bicha</h1>
+          </Center>
+        </Box>
       ))}
     </Grid>
   );
@@ -19,7 +27,7 @@ export default Index;
 
 export const getServerSideProps = async (ctx) => {
   const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=50&offset=200"
+    "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
   );
   const data = await res.json();
 
